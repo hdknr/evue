fd608f6f291d58e9663a1096ef6d0dce926de272
hdknr <gmail@hdknr.com>
2019-02-15 21:26:08 +0900

    sqlite3 and Sequelize

--- docs/sqlite.md
+++ docs/sqlite.md
@@ -3,3 +3,23 @@
 ## sqlite3-offline
 
 - https://github.com/DenisCarriere/sqlite3-offline
+- バージョンを考えると使えないかも。。。。
+
+## sqlite3
+
+- [Electron with Sqlite3 Database – Ashraful Islam – Medium](https://medium.com/@_ashraful/electron-with-sqlite3-database-28b21301fb1)
+
+~~~bash
+$ npm install --save sqlite3
+.
+~~~
+
+~~~bash
+$ which electron-builder
+/Users/hide/.anyenv/envs/ndenv/shims/electron-builder
+
+$ electron-builder install-app-deps
+  • electron-builder version=20.33.2
+  • loaded configuration file=package.json ("build" field)
+  • rebuilding native production dependencies platform=darwin arch=x64
+~~~
--- src/main/db.js
+++ src/main/db.js
@@ -1,29 +1,47 @@
 'use strict'
-import { app } from 'electron'
+import {
+  app
+} from 'electron'
 import path from 'path'
-import sqlite3 from 'sqlite3-offline'
+import Sequelize from 'sequelize'
 
 // https://qiita.com/progre/items/2718f4ad20eecf27d599
+// macOS - $HOME/Library/Application Support/{{ package name }}
+// Windwos - %USERPROFILE%\AppData\Roaming\{{ package name }}
+// pakcage name : run dev で 'Electron'
 const _dbfile = path.join(app.getPath('userData'), 'evue.db')
 
-export async function initDatabase (mainWindow) {
-  const db = new sqlite3.Database(_dbfile)
-
-  db.serialize(function () {
-    db.run('CREATE TABLE lorem (info TEXT)')
-
-    const stmt = db.prepare('INSERT INTO lorem VALUES (?)')
-    for (var i = 0; i < 10; i++) {
-      stmt.run('Ipsum ' + i)
-    }
-    stmt.finalize()
-
-    db.each('SELECT rowid AS id, info FROM lorem', (err, row) => {
-      console.log(err)
-      console.log(row.id + ': ' + row.info)
+function initModels () {
+  const backend = new Sequelize(
+    'eveu', '', '', {
+      dialect: 'sqlite',
+      storage: _dbfile
     })
+
+  const Video = backend.define('video', {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: Sequelize.STRING
   })
-  db.close()
+
+  backend
+    .sync()
+    .then(() => {
+      Video
+        .create({
+          name: 'your name'
+        })
+        .error((err) => {
+          console.log(err)
+        })
+    })
+}
+
+export async function initDatabase (mainWindow) {
+  initModels()
 }
 
 export default {
