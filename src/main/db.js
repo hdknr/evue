e80adf9049c7d239856f58bd164b42fef7c91ca2
hdknr <gmail@hdknr.com>
2019-02-15 15:05:09 +0900

    userData がよい

--- src/main/db.js
+++ src/main/db.js
@@ -3,7 +3,8 @@ import { app } from 'electron'
 import path from 'path'
 import sqlite3 from 'sqlite3-offline'
 
-const _dbfile = path.join(app.getPath('temp'), 'evue.db')
+// https://qiita.com/progre/items/2718f4ad20eecf27d599
+const _dbfile = path.join(app.getPath('userData'), 'evue.db')
 
 export async function initDatabase (mainWindow) {
   const db = new sqlite3.Database(_dbfile)
