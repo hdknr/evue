5bb9f503d5c1a35a1d2ad01944b357564f1c98a5
hdknr <gmail@hdknr.com>
2019-02-15 14:22:15 +0900

    first commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'evue'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
